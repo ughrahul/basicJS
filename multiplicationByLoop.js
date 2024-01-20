a8252b5b2d41ccae6aab57044dfa7ac9cdacbfb2
rr <jhu>
2024-01-20 18:54:21 +0545

    multiplication table by WHILE & DO WHILE loop

---
+++ multiplicationByLoop.js
@@ -0,0 +1,27 @@
+/*MULTIPLICATION TABLE BY FOR LOOP
+const number = parseInt(prompt("ENTER ANY NUMBER FOR MULTIPLICATION TABLE"));
+
+for (let i = 1; i <= 10; i++) {
+  alert(`${number} x ${i} = ${number * i}`);
+}
+
+
+//BY DO WHILE
+const number = 5;
+let i = 1;
+
+do {
+  console.log(`${number} x ${i} = ${number * i}`);
+  i++;
+} while (i <= 10);
+
+//BY WHILE
+const number = 5;
+let i = 1;
+*/
+
+// Example of a while loop to print the multiplication table for 5
+while (i <= 10) {
+  console.log(`${number} x ${i} = ${number * i}`);
+  i++;
+}
